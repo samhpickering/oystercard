6dfe33035caf4012699c7c02e38068f4a1464a9e
Sam Pickering <sam.p@tuta.io>
2022-01-04 16:26:41 +0000

    step 6: added balance limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 class Oystercard
   attr_reader :balance
+  LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
-  def top_up(val)
-    @balance += val
+  def top_up(amount)
+    raise "Top up would take card balance over £#{Oystercard::LIMIT} limit" if @balance + amount > LIMIT
+    
+    @balance += amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,4 +8,9 @@ describe Oystercard do
   it 'adds the top-up value' do
     expect { subject.top_up(10) }.to change { subject.balance }.by(10)
   end
+
+  it 'raised error if top up is over limit' do
+    expect { subject.top_up( Oystercard::LIMIT + 1) }.to raise_error "Top up would take card balance over £#{Oystercard::LIMIT} limit"
+  end
+
 end
