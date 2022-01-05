56247b1f505246fe0bb4b244e4ee959a2ed9dc90
Sam Pickering <sam.p@tuta.io>
2022-01-05 16:38:36 +0000

    step 13: create station class

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,4 +1,17 @@
 require 'station'
 
 describe Station do
+  subject { described_class.new('Picadilly', 2) }
+
+  it 'is initialized with 2 arguments' do
+    expect(Station).to respond_to(:new).with(2).arguments
+  end
+
+  it 'knows its name' do
+    expect(subject.name).to eq('Picadilly')
+  end
+
+  it 'knows its zone' do
+    expect(subject.zone).to eq(2)
+  end
 end
